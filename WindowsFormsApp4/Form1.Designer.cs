889543fc2618f5985ff93e7a92b45a7490760a86
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2023-05-10 23:46:36 +0700

    Add files via upload

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label34 = new System.Windows.Forms.Label();
+            this.label56 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label78 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label57 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(477, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 84);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 352);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1004, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1004, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.TabIndex = 24;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1004, 258);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 16);
+            this.label7.TabIndex = 25;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1004, 352);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 16);
+            this.label8.TabIndex = 26;
+            // 
+            // label15
+            // 
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(477, 163);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(201, 102);
+            this.label15.TabIndex = 27;
+            this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label15.Visible = false;
+            this.label15.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // label34
+            // 
+            this.label34.AutoSize = true;
+            this.label34.Location = new System.Drawing.Point(214, 305);
+            this.label34.Name = "label34";
+            this.label34.Size = new System.Drawing.Size(0, 16);
+            this.label34.TabIndex = 28;
+            this.label34.Visible = false;
+            // 
+            // label56
+            // 
+            this.label56.AutoSize = true;
+            this.label56.Location = new System.Drawing.Point(814, 109);
+            this.label56.Name = "label56";
+            this.label56.Size = new System.Drawing.Size(0, 16);
+            this.label56.TabIndex = 29;
+            this.label56.Visible = false;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(214, 109);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 16);
+            this.label12.TabIndex = 30;
+            this.label12.Visible = false;
+            this.label12.Click += new System.EventHandler(this.label16_Click);
+            // 
+            // label78
+            // 
+            this.label78.AutoSize = true;
+            this.label78.Location = new System.Drawing.Point(814, 305);
+            this.label78.Name = "label78";
+            this.label78.Size = new System.Drawing.Size(0, 16);
+            this.label78.TabIndex = 31;
+            this.label78.Visible = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(292, 209);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 16);
+            this.label13.TabIndex = 32;
+            this.label13.Visible = false;
+            // 
+            // label57
+            // 
+            this.label57.AutoSize = true;
+            this.label57.Location = new System.Drawing.Point(747, 209);
+            this.label57.Name = "label57";
+            this.label57.Size = new System.Drawing.Size(0, 16);
+            this.label57.TabIndex = 33;
+            this.label57.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 555);
+            this.Controls.Add(this.label57);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label78);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label56);
+            this.Controls.Add(this.label34);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label34;
+        private System.Windows.Forms.Label label56;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label78;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label57;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        string[] team = new string[8] { "Real Madrid", "Manchester United", "Arsenal", "Chelsea", "Bayern Munich", "Liverpool", "Manchester City", "Barcelona" };
+        double[] l = new double[8];
+        int a, b;
+        Random generator2 = new Random();
+
+        private void playGames(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    playRound(l[0], l[1], 0, 1);
+                    playRound(l[2], l[3], 2, 3);
+                    playRound(l[4], l[5], 4, 5);
+                    playRound(l[6], l[7], 6, 7);
+                    break;
+                case 1:
+                    playRound(l[0], l[2], 0, 2);
+                    playRound(l[4], l[6], 4, 6);
+                    break;
+                case 2:
+                    playRound(l[0], l[4], 0, 4);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void setWinner(int i, int j, int a)
+        {
+            string labelName = "label" + (i + 1).ToString() + (j + 1).ToString();
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)Controls[labelName];
+            label.Text += team[i] + " (" + a.ToString() + " goals)";
+            label.Visible = true;
+
+            if ((i == 0) && (j == 4))
+            {
+                label.ForeColor = Color.Green;
+            }
+        }
+
+        private void playRound(double l1, double l2, int i, int j)
+        {
+            a = goals(l1, generator2);
+            b = goals(l2, generator2);
+
+            if (a > b)
+            {
+                l[i] = l[i];
+                team[i] = team[i];
+
+                setWinner(i, j, a);
+            }
+
+            if (a < b)
+            {
+                l[i] = l[j];
+                team[i] = team[j];
+
+                setWinner(i, j, b);
+            }
+
+            if (a == b)
+            {
+                playRound(l[i], l[j], i, j);
+            }
+        }
+
+        public int goals(double lambda, Random rnd)
+        {
+            int count = 0;
+            double s = 0;
+
+            while (s > -lambda)
+            {
+                s += Math.Log(rnd.NextDouble());
+                count++;
+            }
+
+            return count - 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random generator = new Random();
+            team = new string[8] { "Real Madrid", "Manchester United", "Arsenal", "Chelsea", "Bayern Munich", "Liverpool", "Manchester City", "Barcelona" };
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label12.Text = "";
+            label34.Text = "";
+            label56.Text = "";
+            label78.Text = "";
+            label13.Text = "";
+            label57.Text = "";
+            label15.Text = "Winner is: \n";
+
+            for (int i = 0; i < 8; i++)
+            {
+                l[i] = generator.Next(1, 10);
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                string labelName = "label" + (i + 1).ToString();
+                System.Windows.Forms.Label label = (System.Windows.Forms.Label)Controls[labelName];
+                label.Text += team[i];
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                playGames(i);
+            }
+
+            button1.Text = "Replay";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
